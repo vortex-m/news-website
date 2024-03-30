f020475324374ec471b176da76cf8f8ab5274f6f
Mayank <mail.mayank001@gmail.com>
2024-03-30 13:43:09 +0530

    First commit

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey = ${API_KEY}`)
     const data = await res.json();
     bindData = (data.articles);
-    
 }
 
 function bindData(articles) {
